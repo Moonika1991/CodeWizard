d92adce8785ae088db47aeb2009ad2d56f4f898f
Moonika1991 <monika9107@gmail.com>
2020-08-19 11:33:13 +0200

    Add files via upload

---
+++ Controller.py
@@ -0,0 +1,19 @@
+import web
+
+urls = (
+    '/', 'home'
+)
+
+render = web.template.render("Views/Templates", base="MainLayout")
+app = web.application(urls, globals())
+
+app = web.application(urls, globals())
+
+#Classes/Routes
+
+class home:
+    def GET(self):
+        return render.Home()
+
+if __name__ == "__main__":
+    app.run()

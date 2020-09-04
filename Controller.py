81cbd5e3319a8eb566de12bf0caa31fd4333966e
Moonika1991 <monika9107@gmail.com>
2020-09-04 09:44:39 +0200

    Add files via upload

--- Controller.py
+++ Controller.py
@@ -1,19 +1,72 @@
 import web
+from Models import RegisterModel, LoginModel
+
+web.config.debug = False
 
 urls = (
-    '/', 'home'
+    '/', 'Home',
+    '/register', 'Register',
+    '/login', 'Login',
+    '/logout', 'Logout',
+    '/postregistration', 'PostRegistration',
+    '/check-login', 'CheckLogin'
 )
 
-render = web.template.render("Views/Templates", base="MainLayout")
 app = web.application(urls, globals())
+session = web.session.Session(app, web.session.DiskStore("sessions"), initializer={'user': None})
+session_data = session._initializer
 
-app = web.application(urls, globals())
+render = web.template.render("Views/Templates", base="MainLayout", globals={'session': session_data, 'current_user':
+                            session_data["user"]})
 
 #Classes/Routes
 
-class home:
+class Home:
     def GET(self):
         return render.Home()
+
+
+class Register:
+    def GET(self):
+        return render.Register()
+
+
+class Login:
+    def GET(self):
+        return render.Login()
+
+
+class PostRegistration:
+    def POST(self):
+        data = web.input()
+
+        reg_model = RegisterModel.RegisterModel()
+        reg_model.insert_user(data)
+
+        return data.username
+
+
+class CheckLogin:
+    def POST(self):
+        data = web.input()
+        login = LoginModel.LoginModel()
+        login.check_user(data)
+        isCorrect = login.check_user(data)
+
+        if isCorrect:
+            session_data["user"] = isCorrect
+            return isCorrect
+
+        return "error"
+
+
+class Logout:
+    def GET(self):
+        session['user'] = None
+        session_data['user'] = None
+
+        session.kill()
+        return "success"
 
 if __name__ == "__main__":
     app.run()
